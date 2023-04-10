f40a09c23ae8dc086d36d925080c26e349337445
Harut <harutune7@gmail.com>
2023-04-10 14:38:57 +0300

    Update routes.py

--- app/api/routes.py
+++ app/api/routes.py
@@ -10,7 +10,7 @@ async def create_user(request):
 
 
 def add_routes(app):
-    app.router.add_route('GET', r'/v1/user', create_user, name='create_user')
+    app.router.add_route('POST', r'/v1/user', create_user, name='create_user')
     app.router.add_route('POST', r'/v1/user/{id}/balance', get_user_balance, name='get_user')
     app.router.add_route('PUT', r'/v1/transaction', add_transaction, name='add_transaction')
     app.router.add_route('GET', r'/v1/transaction/{id}', get_transaction, name='incoming_transaction')
